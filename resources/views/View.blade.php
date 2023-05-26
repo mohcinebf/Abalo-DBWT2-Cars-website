7fe28b2e8dd1a64348de86548e12d61d19d57095
Mohcine Boujraf <mohssine441@gmail.com>
2023-05-26 15:38:10 +0200

    Test

--- resources/views/View.blade.php
+++ resources/views/View.blade.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="{{asset('css/article.css')}}">
 </head>
 <body>
-<!--test Git branche-->
     <div id="header">
         <div class="logo">
         <img src="logo.jpg" width="80px">
